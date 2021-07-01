850598cf88a58c498d7f9fba357ebc8a3b58a13d
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-06-30 20:41:54 -0400

    various improvements

--- index.js
+++ index.js
@@ -1,35 +1,13 @@
-require('@babel/register')({
-  plugins: [
-  ].map(require.resolve),
-  presets: [
-    '@babel/preset-env',
-    '@babel/preset-react'
-  ].map(require.resolve)
-})
-
-const fs = require('fs')
 const puppeteer = require('puppeteer')
-const { Readable } = require('stream')
-const path = require('path')
-const { createElement: h } = require('react')
-const { renderToStaticMarkup } = require('react-dom/server')
-const Datauri = require('datauri')
-const resolveCWD = require('resolve-cwd')
+const DataURI = require('datauri')
 
 const baseCSS = `*{box-sizing:border-box}body{margin:0;font-family:system-ui,sans-serif}`
 
-const getHtmlData = ({
-  body,
-  baseCSS,
-  css,
-  styles,
-  webfont
-}) => {
+const getHtmlData = ({ body, css = '', webfont }) => {
   const fontCSS = webfont ? getWebfontCSS(webfont) : ''
   const html = `<!DOCTYPE html>
     <head>
     <meta charset="utf-8"><style>${baseCSS}${fontCSS}${css}</style>
-    ${styles}
     </head>
     <body style="display:inline-block">
     ${body}`
@@ -37,46 +15,33 @@ const getHtmlData = ({
 }
 
 const getWebfontCSS = (fontpath) => {
-  const { content } = new Datauri(fontpath)
-  const [ name, ext ] = fontpath.split('/').slice(-1)[0].split('.')
-  const css = (`@font-face {
+  const { content } = new DataURI(fontpath)
+  const [name] = fontpath.split('/').slice(-1)[0].split('.')
+  const css = `@font-face {
   font-family: '${name}';
   font-style: normal;
   font-weight: 400;
   src: url(${content});
-}`)
+}`
   return css
 }
 
-module.exports = async (Component, opts = {}) => {
-  const {
-    css = '',
-    filename,
-    outDir,
-    webfont,
-    type = 'png', // jpeg, png and pdf are allowed
-  } = opts
+module.exports = async (body, opts = {}) => {
+  const html = body.includes('<head>')
+    ? body
+    : getHtmlData({
+        body,
+        css: opts.css,
+        webfont: opts.webfont,
+      })
 
-  const props = Object.assign({
-    width: opts.width,
-    height: opts.height,
-  }, opts.props)
-
-  let styles = ''
-  const el = h(Component, props)
-  const body = renderToStaticMarkup(el)
-
-  const html = getHtmlData({
-    body,
-    baseCSS,
-    css,
-    styles,
-    webfont
-  })
-
-  const browser = await puppeteer.launch(opts.puppeteer)
+  const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.setContent(html)
+
+  await page.setContent(html, {
+    waitUntil: ['domcontentloaded', 'networkidle0'],
+    timeout: 0,
+  })
 
   let rect = {}
   if (!opts.width && !opts.height) {
@@ -91,26 +56,17 @@ module.exports = async (Component, opts = {}) => {
     height,
   })
 
-  let result
-  if (type === 'pdf') {
-    result = await page.pdf({
+  const result = await page.screenshot({
+    type: 'png',
+    clip: {
+      x: 0,
+      y: 0,
       width,
       height,
-    })
-  } else {
-    result = await page.screenshot({
-      type: type,
-      clip: {
-        x: 0,
-        y: 0,
-        width,
-        height,
-      },
-      omitBackground: true
-    })
-  }
+    },
+    omitBackground: true,
+  })
 
   await browser.close()
-
   return result
 }
